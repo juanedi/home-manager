ef3cd12cc5853b7faf7ca79fb39d5e269852fc03
Juan Edi <jedi11235@gmail.com>
2026-02-15 13:28:55 -0300

    install texlive + pandoc

--- home.nix
+++ home.nix
@@ -34,6 +34,8 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    pkgs.pandoc
+    pkgs.texlive.combined.scheme-full
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
