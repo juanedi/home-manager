febc7919c5fa4673357a7a25c6d364b7759399e1
Juan Edi <jedi11235@gmail.com>
2026-02-20 09:13:49 -0300

    bring over hyprland config files to home-manager setup

--- home.nix
+++ home.nix
@@ -53,6 +53,12 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".config/hypr/monitors.conf".source = programs/hypr/monitors.conf;
+    ".config/hypr/input.conf".source = programs/hypr/input.conf;
+    ".config/hypr/bindings.conf".source = programs/hypr/bindings.conf;
+    ".config/hypr/looknfeel.conf".source = programs/hypr/looknfeel.conf;
+    ".config/hypr/autostart.conf".source = programs/hypr/autostart.conf;
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
