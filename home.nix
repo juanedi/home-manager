691731ede37aa964bf832f1791c03fa74f3ffc81
Juan Edi <jedi11235@gmail.com>
2026-02-21 00:45:10 -0300

    recursive copy so we don't have to list all files

--- home.nix
+++ home.nix
@@ -53,15 +53,15 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/hypr/monitors.conf".source = dotfiles/hypr/monitors.conf;
-    ".config/hypr/input.conf".source = dotfiles/hypr/input.conf;
-    ".config/hypr/bindings.conf".source = dotfiles/hypr/bindings.conf;
-    ".config/hypr/looknfeel.conf".source = dotfiles/hypr/looknfeel.conf;
-    ".config/hypr/autostart.conf".source = dotfiles/hypr/autostart.conf;
+    ".config" = {
+      source = ./dotfiles;
+      recursive = true;
+    };
 
-    ".local/share/applications/Claude.desktop".source = app-launchers/Claude.desktop;
-    ".local/share/applications/Chess.com.desktop".source = app-launchers/Chess.com.desktop;
-    ".local/share/applications/Telegram.desktop".source = app-launchers/Telegram.desktop;
+    ".local/share/applications" = {
+      source = ./app-launchers;
+      recursive = true;
+    };
 
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
