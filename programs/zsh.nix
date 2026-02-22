e51e10a44c3ce7bee023f0eb490d33c1031688bb
Juan Edi <juan@noredink.com>
2026-02-22 20:33:32 -0300

    more missing config from macos set up

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -11,9 +11,13 @@
       eps   = "ps aux | grep -i";
       gst   = "git status";
       doom  = "~/.emacs.d/bin/doom";
+      sshk = "kitty +kitten ssh";
     } // lib.optionalAttrs pkgs.stdenv.isLinux {
       pbcopy  = "wl-copy -n";
       pbpaste = "wl-paste";
+    } // lib.optionalAttrs pkgs.stdenv.isDarwin {
+      lstcp = "lsof -i -n -P | grep TCP | grep LISTEN";
+      claude-personal = "CLAUDE_CONFIG_DIR=~/.claude-personal ~/npm-installs/node_modules/@anthropic-ai/claude-code/cli.js";
     };
 
     oh-my-zsh = {
@@ -22,6 +26,8 @@
 
     sessionVariables = {
       DOOMDIR = "$HOME/.config/doom";
+    } // lib.optionalAttrs pkgs.stdenv.isDarwin {
+      EMACS = "/Applications/Emacs-29.4.app/Contents/MacOS/Emacs";
     };
 
     initContent = ''
