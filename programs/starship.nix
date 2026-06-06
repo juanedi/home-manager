19d725e3ca7a1f88f46eb09f4ece2c7dd2776ac0
Juan Edi <juan@noredink.com>
2026-06-06 00:32:03 -0300

    add firstaide status to starship

--- programs/starship.nix
+++ programs/starship.nix
@@ -11,11 +11,18 @@
         "$directory"
         "$git_branch"
         "$git_state"
+        "\${custom.firstaide}"
         "$character"
       ];
       hostname = {
         ssh_only = true;
         format = "[@$hostname](bold blue) ";
+      };
+      custom.firstaide = {
+        when = "command -v firstaide && ! firstaide status";
+        symbol = "";
+        style = "fg:red";
+        format = "[$symbol]($style) ";
       };
     };
   };
